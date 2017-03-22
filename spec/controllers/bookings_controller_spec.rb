154351e9f82d9d926d2fb34c262e233ff24e9b3d
Sonia Lin <sonia_lin@hotmail.com>
2017-03-22 17:32:56 -0500

    Updated client with multiple requests for proper error rescue

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -15,25 +15,32 @@ RSpec.describe BookingsController, :type => :controller do
   end
 
   describe "GET #show" do
-    it "responds to multiple show page requests successfully with an HTTP 200 status code" do
+    it "responds to multiple show page requests with error handling" do
       start_time = Time.now
       end_time = nil
       number_of_requests = 0
+      number_of_errors = 0
       
       2000.times do
-        number_of_requests += 1
-        RestClient.get 'http://localhost:3000/bookings/1'
+        number_of_requests += 1       
+        begin
+          RestClient.get 'http://localhost:3000/bookings/1'
+        rescue
+          puts 'Something is wrong.'
+          number_of_errors += 1
+        end     
       end
 
       end_time = Time.now
       puts "Number of requests made is #{number_of_requests}"
+      puts "Number of errors encountered is #{number_of_errors}"
       puts "Start time is #{start_time}"
       puts "End time is #{end_time}"
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
-    it "responds to multiple show page requests with error handling" do
+    it "responds to multiple show page requests with error threshold" do
       start_time = Time.now
       end_time = nil
       number_of_requests = 0
