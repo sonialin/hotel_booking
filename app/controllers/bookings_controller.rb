b134509dc4888a3ba7c793928391a42393728826
Sonia Lin <sonia_lin@hotmail.com>
2017-04-25 22:18:46 -0500

    Changed db shard selection for read queries to be generated from a probability prioritizing current location

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,16 +2,16 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: [:show]
 
   BAD_REQUEST_SELECTION = 2 #randomly designated number out of 10 that stipulates an error should be thrown
-  BAD_REQUEST_SELECTION2 = [1]
   CURRENT_LOCATION = "europe"
+  ALL_LOCATIONS = ["america", "europe", "asia", "others"]
 
   def index
     request_selection = (1..10).to_a.sample
 
-    if BAD_REQUEST_SELECTION2.include? request_selection
+    if BAD_REQUEST_SELECTION.include? request_selection
       raise 'error'
     else
-      @bookings = Booking.using(CURRENT_LOCATION.to_sym).all
+      @bookings = Booking.using(get_random_location.to_sym).all
       render json: @bookings
     end
   end
@@ -45,10 +45,19 @@ class BookingsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_booking
-      @booking = Booking.using(CURRENT_LOCATION.to_sym).find(params[:id])
+      @booking = Booking.using(get_random_location.to_sym).find(params[:id])
     end
 
     def booking_params
       params.require(:booking).permit(:user_id, :room_id, :comment, :check_out_on, :check_in_on)
+    end
+
+    def get_random_location
+      case rand(1..100)
+      when  1..50
+        CURRENT_LOCATION
+      else
+        ALL_LOCATIONS.delete_if {|x| x == CURRENT_LOCATION }.sample
+      end
     end
 end
